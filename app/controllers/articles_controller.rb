af969c141615ec2b789c53ed828e4e48b038732c
Kevin Prosser <kevin.prosser1@yahoo.com>
2020-09-05 19:16:38 -0400

    deleted private section and line under class in articlescontroller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:show, :edit, :update, :destroy]
   # GET /articles
   # GET /articles.json
   def index
@@ -10,9 +9,5 @@ class ArticlesController < ApplicationController
 
     end
 
-private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+
 end
