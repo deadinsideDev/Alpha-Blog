338c242c285bf7611ed05415ea94944638b0705b
Kevin Prosser <kevin.prosser1@yahoo.com>
2020-09-09 20:17:08 -0400

    added links such as home page about page and articles listing, also added edit and delete buttons in article listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-   resources :articles, only: [:show, :index, :new, :create, :edit, :update] 
-  # get 'articles', to: 'articles#show'
+   resources :articles
+  
 end
