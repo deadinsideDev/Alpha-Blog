7cb9eb890782c214901ccc9450ccaa388b5d51eb
Kevin Prosser <kevin.prosser1@yahoo.com>
2020-09-12 13:47:59 -0400

    cleaned up code by creating partials and methods in application_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
-  # GET /articles
-  # GET /articles.json
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @articles = Article.all
   end
 
     def show
-     @article = Article.find(params[:id])
     end
 
     def new
@@ -14,11 +14,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit 
-      @article = Article.find(params[:id])
     end
 
     def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
+      @article = Article.new(article_params)
       if @article.save
         flash[:notice] = "Article was created succesfully."
         redirect_to @article
@@ -28,8 +27,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+      if @article.update(article_params)
         flash[:notice] = "Article was updated succesfully."
         redirect_to @article
       else
@@ -38,8 +36,20 @@ class ArticlesController < ApplicationController
     end
 
     def destroy  
-      @article = Article.find(params[:id])
       @article.destroy 
-      redirect_to articles_path
+      redirect_to articles_path    
     end
+
+
+    
+    private 
+
+    def set_article
+    @article = Article.find(params[:id])
+    end
+
+    def article_params 
+      params.require(:article).permit(:title, :description)
+    end
+
 end
